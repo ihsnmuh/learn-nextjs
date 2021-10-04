cd4ba494a831681ce18376c821c7fac8795234f0
Muhammad Ihsan <muhammadihsan076@gmail.com>
2021-10-04 12:02:46 +0700

    adding static side generation on Home Page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,21 @@
 import EventList from '../components/events/event-list';
 import { getFeaturedEvents } from '../dummy-data';
 
-export default function Home() {
-  const featuredEvents = getFeaturedEvents();
+export default function Home(props) {
+  const { events } = props;
 
   return (
     <div>
-      <EventList items={featuredEvents} />
+      <EventList items={events} />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+  return {
+    props: {
+      events: featuredEvents,
+    },
+  };
 }

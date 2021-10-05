f5e115b8eec5fec45177041f7c882ce2d49112a3
Muhammad Ihsan <muhammadihsan076@gmail.com>
2021-10-05 12:24:11 +0700

    reusing inside component

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -8,10 +8,9 @@ import Button from '../../components/ui/button';
 import ErrorAlert from '../../components/ui/error-alert';
 
 export default function FilteredEventPage() {
-  const [loadedEvents, setLoadedEvents] = useState([]);
   const router = useRouter();
-
   const filterData = router.query.slug;
+  const [loadedEvents, setLoadedEvents] = useState([]);
 
   const { data, error } = useSWR(
     'https://nextjs-learn-8ad33-default-rtdb.firebaseio.com/events.json'
@@ -41,6 +40,16 @@ export default function FilteredEventPage() {
   const numYear = +filteredYear;
   const numMonth = +filteredMonth;
 
+  const pageHeadLoad = (
+    <Head>
+      <title>{`Filter Events ${numMonth} - ${numYear}`}</title>
+      <meta
+        name='description'
+        content={`All Events for ${numMonth} - ${numYear}`}
+      />
+    </Head>
+  );
+
   if (
     isNaN(numYear) ||
     isNaN(numMonth) ||
@@ -52,13 +61,7 @@ export default function FilteredEventPage() {
   ) {
     return (
       <>
-        <Head>
-          <title>{`Filter Events ${numMonth} - ${numYear}`}</title>
-          <meta
-            name='description'
-            content={`All Events for ${numMonth} - ${numYear}`}
-          />
-        </Head>
+        {pageHeadLoad}
         <ErrorAlert>
           <p className='center'>Invalid filter. Please adjust your values!</p>
         </ErrorAlert>
@@ -80,13 +83,7 @@ export default function FilteredEventPage() {
   if (!filteredEvents || filteredEvents.length === 0) {
     return (
       <>
-        <Head>
-          <title>{`Filter Events ${numMonth} - ${numYear}`}</title>
-          <meta
-            name='description'
-            content={`All Events for ${numMonth} - ${numYear}`}
-          />
-        </Head>
+        {pageHeadLoad}
         <ErrorAlert>
           <p className='center'>No events found for your filter!</p>
         </ErrorAlert>
@@ -101,13 +98,7 @@ export default function FilteredEventPage() {
 
   return (
     <>
-      <Head>
-        <title>{`Filter Events ${numMonth} - ${numYear}`}</title>
-        <meta
-          name='description'
-          content={`All Events for ${numMonth} - ${numYear}`}
-        />
-      </Head>
+      {pageHeadLoad}
       <ResultsTitle date={date} />
       <EventList items={filteredEvents} />
     </>

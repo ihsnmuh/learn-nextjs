2d0d73139713c760dd9f385e932c6ec1540b5715
Muhammad Ihsan <muhammadihsan076@gmail.com>
2021-10-08 15:34:27 +0700

    adding more error handler

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,17 +1,4 @@
-import { MongoClient } from 'mongodb';
-
-async function connectDatabase() {
-  const url =
-    'mongodb+srv://User:xV!NvYPsxbe9BJd@cluster0.yfbud.mongodb.net/newsLetters?retryWrites=true&w=majority';
-  const client = await MongoClient.connect(url);
-
-  return client;
-}
-
-async function insertDocument(client, document) {
-  const db = client.db();
-  await db.collection('newsletter').insertOne(document);
-}
+import { connectDatabase, insertDocument } from '../../helpers/db-util';
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
@@ -32,7 +19,7 @@ export default async function handler(req, res) {
     }
 
     try {
-      await insertDocument(client, { email: userEmail });
+      await insertDocument(client, 'newsLetters', { email: userEmail });
       client.close();
     } catch (error) {
       res.status(500).json({ message: 'Inserting data failed!' });

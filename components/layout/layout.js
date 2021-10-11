36946cd8af882fec7dbab9bde72548001702255a
Muhammad Ihsan <muhammadihsan076@gmail.com>
2021-10-11 10:46:05 +0700

    using context data in component

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,10 +1,21 @@
 import MainHeader from './main-header';
+import Notification from '../ui/notification';
+import NotificationContext from '../../store/notification-context';
 
 export default function Layout(props) {
+  const notificationctx = useContext(NotificationContext);
+
+  const activeNotification = notificationctx.notification;
+
   return (
     <>
       <MainHeader />
       <main>{props.children}</main>
+      <Notification
+        title={activeNotification.title}
+        message={activeNotification.message}
+        status={activeNotification.status}
+      />
     </>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,27 @@
-import Layout from '../components/layout/layout';
-import '../styles/globals.css';
 import Head from 'next/head';
+import Layout from '../components/layout/layout';
+import { NotificationContextProvider } from '../store/notification-context';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Layout>
-        <Head>
-          <title>Learning NextJs</title>
-          <meta
-            name='description'
-            content='Halaman ini hanya berisi desciption latihan Next.......'
-          />
-          <meta
-            name='viewport'
-            content='initial-scale=1.0, width=device-width'
-          />
-        </Head>
-        <Component {...pageProps} />
-      </Layout>
+      <NotificationContextProvider>
+        <Layout>
+          <Head>
+            <title>Learning NextJs</title>
+            <meta
+              name='description'
+              content='Halaman ini hanya berisi desciption latihan Next.......'
+            />
+            <meta
+              name='viewport'
+              content='initial-scale=1.0, width=device-width'
+            />
+          </Head>
+          <Component {...pageProps} />
+        </Layout>
+      </NotificationContextProvider>
     </>
   );
 }

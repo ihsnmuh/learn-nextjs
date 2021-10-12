e82723c8fa0662492e628dabca4f5e4aa739f32c
Muhammad Ihsan <muhammadihsan076@gmail.com>
2021-10-12 11:45:51 +0700

    adding Hero component

---
+++ components/home-page/hero.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import classes from './hero.module.css';
+import Image from 'next/image';
+
+export default function Hore() {
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src='/images/side/ihsan-image.png'
+          alt='image Muhammad Ihsan'
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hi, I'm Ihsan</h1>
+      <p>
+        My blog about web development - especially frontend framework like React
+      </p>
+    </section>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,10 @@
+import Hero from '../components/home-page/hero';
+
 export default function HomePage() {
-  return <div></div>;
+  return (
+    <>
+      <Hero />
+      {/* <FeaturedPosts /> */}
+    </>
+  );
 }

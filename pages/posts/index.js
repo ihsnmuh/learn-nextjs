32dbe9104e841ee7ff0f8901dce6549f59843288
Muhammad Ihsan <muhammadihsan076@gmail.com>
2021-10-13 13:31:02 +0700

    adding allposts page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,36 @@
+import AllPosts from '../../components/post/all-posts';
+
+const DUMMY_POSTS = [
+  {
+    slug: 'first-blog',
+    title: 'My First Blog',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'ini adalah blog saya yang pertama',
+    date: '2021-7-19',
+  },
+  {
+    slug: 'second-blog',
+    title: 'My Second Blog',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'ini adalah blog saya yang kedua',
+    date: '2021-7-19',
+  },
+  {
+    slug: 'third-blog',
+    title: 'My Third Blog',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'ini adalah blog saya yang ketiga',
+    date: '2021-7-19',
+  },
+  {
+    slug: 'fourth-blog',
+    title: 'My Fourth Blog',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'ini adalah blog saya yang keempat',
+    date: '2021-7-19',
+  },
+];
+
 export default function AllPostPage() {
-  return <div></div>;
+  return <AllPosts posts={DUMMY_POSTS} />;
 }

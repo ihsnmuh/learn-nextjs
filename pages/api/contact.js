c625ce0734c735f96d4aa73bad221b1534860caf
Muhammad Ihsan <muhammadihsan076@gmail.com>
2021-10-18 11:09:26 +0700

    Store message with MongoDB

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const { email, name, message } = req.body;
 
@@ -21,6 +23,28 @@ function handler(req, res) {
       message,
     };
 
+    let client;
+
+    try {
+      client = await MongoClient.connect(
+        'mongodb+srv://User:xV!NvYPsxbe9BJd@cluster0.yfbud.mongodb.net/mySite?retryWrites=true&w=majority'
+      );
+    } catch (error) {
+      res.status(500).json({ message: 'Cant connect to database!' });
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection('message').insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: 'Storing message failed!' });
+      return;
+    }
+    client.close();
     res
       .status(201)
       .json({ message: 'Successfully adding message!', message: newMessage });
